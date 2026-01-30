dbba1d9bd3653bc3f337de470b9d18e56b6200ae
kimson751 <kimson751@gmail.com>
2026-01-30 15:45:17 +0700

    k

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Dung';
+const giftUrl = 'http://youtobe.com';
+const eventName = 'Chúc Mừng Valentine';
+const titleCard = 'Tặng EM';
+const contentCard = 'Gửi Em: Anh không giỏi trong việc chọn quà nhưng hy vọng em sẽ thích món quà này và chúc em một ngày Valentine thật ý nghĩa luôn ấm áp, vui vẻ, hạnh phúc và những điều tốt đẹp nhất sẽ đến với em. Cám ơn em !            "tiểu thần tiên" -->.💘';
+
+
+const giftImage = 'em1.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
